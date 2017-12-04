3963d738e7d2daa01d76412dfbcae49bf1b05b3c
bcarvajal <axl.carvajal@gmail.com>
2017-12-04 11:00:40 -0300

    Update variables.js

--- static/scripts/variables.js
+++ static/scripts/variables.js
@@ -15,7 +15,7 @@ var plotColors;
 
 
 var webstaticURL = "static";
-var webserviceURL = "http://http://wineai.ddns.net/BackEnd";
+var webserviceURL = "http://wineai.ddns.net/BackEnd";
 
 
 var devcounter;
